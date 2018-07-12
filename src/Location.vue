022dac6123543050ad5f344febaedd0d57fccccf
Matthew Ess <mess@yelp.com>
2018-07-12 13:02:13 -0400

    Add ceremony and reception times

--- src/Location.vue
+++ src/Location.vue
@@ -3,14 +3,14 @@
   .columns
     .column
       .notification.is-primary
-        h1.title ceremony at
-        h2.subtitle west park
+        h1.title ceremony
+        h2.subtitle at 6pm in west park
         a.subtitle(href='https://goo.gl/maps/bXWhhLQevHH2') 2700 w. 116th st, carmel, in 46044
         img(src='./assets/west-park.jpg')
     .column
       .notification.is-primary.fill
-        h1.title reception at
-        h2.subtitle the ritz charles
+        h1.title reception
+        h2.subtitle at 630pm at the ritz charles
         a.subtitle(href='https://goo.gl/ZyHt1m') 12156 n. meridian st, carmel, in 46032#[br]
         img(src='./assets/garden-pavilion-small.jpg')
         a.has-text-weight-bold.is-size-4(href='https://goo.gl/forms/bJFwNb0qNHE36ozg2') request songs here!
